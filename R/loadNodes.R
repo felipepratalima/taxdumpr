a1c13bbab4ba6dc5d9f7c379d3c6459f66476810
Felipe Prata Lima <felipepratalima@gmail.com>
2018-08-15 19:42:14 -0300

    loadNodes update

--- R/loadNodes.R
+++ R/loadNodes.R
@@ -2,5 +2,9 @@ loadNodes <- function(nodes.dmp.location = "./nodes.dmp") {
   nodes.dmp <- read.table(nodes.dmp.location, header = F, sep = "|", strip.white = T, fill = T, stringsAsFactors = F)
   nodes.dmp <- nodes.dmp[,1:3]
   names(nodes.dmp) <- c("id", "parentId", "rank")
+  ## update
+  nodes.dmp <- subset(nodes.dmp, id != parentId)
+  ## --
+
   return(nodes.dmp)
 }
